1ffdde6097c2dcfd0b6c4884504d75d44ef67e3d
rpask00 <rp.261000@gmail.com>
2020-10-04 12:25:17 +0200

    map fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDNtLpeBuJr69aljVdfAanu9jKo9AGSg30",
-    authDomain: "ezsat-b3013.firebaseapp.com",
-    databaseURL: "https://ezsat-b3013.firebaseio.com",
-    projectId: "ezsat-b3013",
-    storageBucket: "ezsat-b3013.appspot.com",
-    messagingSenderId: "728094012649",
-    appId: "1:728094012649:web:18013b61e2137a3d"
-  }
+  firebaseConfig: {}
 };
 
 /*
